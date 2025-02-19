d2459aaab297db889bff46c11f7cec2fa97ff9e9
R-adi <103630449+R-adi@users.noreply.github.com>
2025-02-19 15:47:03 +0530

    jj

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -4,7 +4,8 @@ dotenv.config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect("mongodb+srv://codadity:nkzydw1KGvnJtbaY@cluster0.pl1lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
